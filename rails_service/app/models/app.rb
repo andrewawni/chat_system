53fbf7d3ae3bc2a765815d52bb32bd063bd63cfc
andrewawni <andrewawnii@gmail.com>
2021-02-16 18:18:42 +0200

    [feat]: hide ids and timestamps from resources

--- rails_service/app/models/app.rb
+++ rails_service/app/models/app.rb
@@ -1,4 +1,9 @@
 class App < ApplicationRecord
   validates :token, presence: true
   has_many :chats
+
+  def as_json(options = {})
+    super(options.merge({ only: [:token, :name] }))
+  end
+  
 end
--- rails_service/app/models/chat.rb
+++ rails_service/app/models/chat.rb
@@ -2,4 +2,8 @@ class Chat < ApplicationRecord
   validates :number, presence: true
   belongs_to :app
   has_many :messages
+
+  def as_json(options = {})
+    super(options.merge({ only: [:number, :name] }))
+  end
 end
